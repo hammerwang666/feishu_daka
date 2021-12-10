746f4344bcbd8be1805993e4cc7e1dbf18d80a73
zengfan <zengfan@guorou.tv>
2021-12-10 18:46:09 +0800

    feat: img

--- feishu.air/feishu.py
+++ feishu.air/feishu.py
@@ -37,11 +37,23 @@ if exists(Template(r"tpl1634293506221.png", record_pos=(0.011, 0.561), resolutio
     touch(Template(r"tpl1634293519191.png", record_pos=(-0.209, 0.558), resolution=(1080, 1920)))
 
 
-touch(Template(r"tpl1633933927403.png", record_pos=(-0.085, 0.805), resolution=(1080, 1920)))
+if exists(Template(r"tpl1633933927403.png", record_pos=(-0.085, 0.805), resolution=(1080, 1920))):
+    touch(Template(r"tpl1633933927403.png", record_pos=(-0.085, 0.805), resolution=(1080, 1920)))
+    
+if exists(Template(r"tpl1639044985375.png", record_pos=(-0.082, 1.029), resolution=(720, 1600))): 
+    touch(Template(r"tpl1639044985375.png", record_pos=(-0.082, 1.029), resolution=(720, 1600)))
+
 
 sleep(5)
 
-touch(Template(r"tpl1632813414657.png", record_pos=(-0.114, -0.017), resolution=(1080, 1920)))
+if exists(Template(r"tpl1632813414657.png", record_pos=(-0.114, -0.017), resolution=(1080, 1920))):
+    touch(Template(r"tpl1632813414657.png", record_pos=(-0.114, -0.017), resolution=(1080, 1920)))
+
+    
+if exists(Template(r"tpl1639132918918.png", record_pos=(-0.125, -0.217), resolution=(720, 1600))): 
+    
+    touch(Template(r"tpl1639132918918.png", record_pos=(-0.125, -0.217), resolution=(720, 1600)))
+
 
 sleep(20)
 
