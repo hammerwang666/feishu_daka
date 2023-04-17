018df9acc3f106325b561a2787dbeb3f975c23ae
zengfan <zfgdmz@gmail.com>
2023-04-17 12:56:26 +0800

    feat: path

--- daka_airtest.sh
+++ daka_airtest.sh
@@ -1,4 +1,4 @@
-#sleep $[ ( $RANDOM % 200 )  + 1 ]
+sleep $[ ( $RANDOM % 200 )  + 1 ]
 
 "/Applications/AirtestIDE.app/Contents/MacOS/AirtestIDE" runner "/Users/zf/workspace/feishu_daka/feishu.air"  --device android://127.0.0.1:5037/$1?cap_method=MINICAP&&ori_method=MINICAPORI&&touch_method=MINITOUCH
 
--- feishu.sh
+++ feishu.sh
@@ -1,3 +1,3 @@
-# sleep $[ ( $RANDOM % 720 )  + 1 ]
+sleep $[ ( $RANDOM % 720 )  + 1 ]
 "/Applications/AirtestIDE.app/Contents/MacOS/AirtestIDE" runner "/Users/zf/workspace/feishu_daka/feishu.air"  --device android://127.0.0.1:5037/$1?cap_method=MINICAP&&ori_method=MINICAPORI&&touch_method=MINITOUCH
 
